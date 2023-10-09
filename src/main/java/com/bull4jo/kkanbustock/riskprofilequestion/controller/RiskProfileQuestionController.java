06f8870ced3a9a4989400d59e20d99e58783c7f3
ghdeo <goldencorn7@naver.com>
2023-10-09 21:59:53 +0900

    [FEAT] RiskProfileQuestionController 응답 ResponseEntity 사용 (#5)

--- src/main/java/com/bull4jo/kkanbustock/riskprofilequestion/controller/RiskProfileQuestionController.java
+++ src/main/java/com/bull4jo/kkanbustock/riskprofilequestion/controller/RiskProfileQuestionController.java
@@ -4,6 +4,7 @@ import com.bull4jo.kkanbustock.riskprofilequestion.controller.dto.RiskProfileQue
 import com.bull4jo.kkanbustock.riskprofilequestion.domain.InvestorType;
 import com.bull4jo.kkanbustock.riskprofilequestion.service.RiskProfileQuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,12 +16,14 @@ public class RiskProfileQuestionController {
     private final RiskProfileQuestionService riskProfileQuestionService;
 
     @GetMapping("/v1/risk-profile-questions")
-    public List<RiskProfileQuestionResponse> getRiskProfileQuestion() {
-        return riskProfileQuestionService.findAll();
+    public ResponseEntity<List<RiskProfileQuestionResponse>> getRiskProfileQuestion() {
+        return ResponseEntity
+                .ok(riskProfileQuestionService.findAll());
     }
 
     @PostMapping("/v1/risk-profile-questions")
-    public InvestorType analyzeQuestionResults(@RequestBody int[] surveyResponses) {
-        return riskProfileQuestionService.analyzeQuestionResults(surveyResponses);
+    public ResponseEntity<InvestorType> analyzeQuestionResults(@RequestBody boolean[] surveyResponses) {
+        return ResponseEntity
+                .ok(riskProfileQuestionService.analyzeQuestionResults(surveyResponses));
     }
 }
