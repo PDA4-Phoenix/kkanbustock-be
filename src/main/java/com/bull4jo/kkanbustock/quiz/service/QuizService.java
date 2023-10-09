291b179edfea0778e777b603d499f62c87e827f8
2522001 <minseo770@gmail.com>
2023-10-10 00:40:01 +0900

    [FEAT] QuizService 정의 (#3)

--- src/main/java/com/bull4jo/kkanbustock/quiz/service/QuizService.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/service/QuizService.java
@@ -1,4 +1,52 @@
 package com.bull4jo.kkanbustock.quiz.service;
 
+import com.bull4jo.kkanbustock.member.domain.entity.Member;
+import com.bull4jo.kkanbustock.member.repository.MemberRepository;
+import com.bull4jo.kkanbustock.quiz.domain.entity.SolvedStockQuiz;
+import com.bull4jo.kkanbustock.quiz.domain.entity.StockQuiz;
+import com.bull4jo.kkanbustock.quiz.domain.request.QuizRequest;
+import com.bull4jo.kkanbustock.quiz.domain.response.QuizResponse;
+import com.bull4jo.kkanbustock.quiz.repository.QuizRepository;
+import com.bull4jo.kkanbustock.quiz.repository.SolvedQuizRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class QuizService {
+
+    private final QuizRepository quizRepository;
+    private final MemberRepository memberRepository;
+    private final SolvedQuizRepository solvedQuizRepository;
+
+    public QuizResponse getQuiz(long quizId) {
+        StockQuiz stockQuiz = quizRepository.findById(quizId).orElseThrow();
+        return QuizResponse.of(stockQuiz);
+    }
+
+    public List<QuizResponse> getQuizzes() {
+        List<StockQuiz> quizzes = quizRepository.findAll();
+        return quizzes.stream().map(QuizResponse::of).toList();
+    }
+
+    public void saveSolvedQuiz(QuizRequest quizRequest) {
+        // QuizRequest에서 필요한 정보 추출
+        Long memberId = quizRequest.getMemberId();
+        Long stockQuizId = quizRequest.getStockQuizId();
+        Date solveDate = quizRequest.getSolveDate();
+        Boolean isCorrect = quizRequest.getIsCorrect();
+
+        // Member와 StockQuiz 엔티티 가져오기
+        Member member = memberRepository.findById(memberId).orElseThrow();
+        StockQuiz stockQuiz = quizRepository.findById(stockQuizId).orElseThrow();
+
+        // SolvedStockQuiz 엔티티 생성
+        SolvedStockQuiz solvedStockQuiz = new SolvedStockQuiz(member, stockQuiz, solveDate, isCorrect);
+
+        // SolvedStockQuiz 저장
+        solvedQuizRepository.save(solvedStockQuiz);
+    }
 }
