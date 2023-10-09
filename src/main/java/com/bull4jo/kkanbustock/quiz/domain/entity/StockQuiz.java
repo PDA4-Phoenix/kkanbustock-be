7c7200a5a714ac7c7d80966797271faaa7a30584
2522001 <minseo770@gmail.com>
2023-10-09 16:56:13 +0900

    [FEAT] StockQuiz entity 정의

--- src/main/java/com/bull4jo/kkanbustock/quiz/domain/entity/StockQuiz.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/domain/entity/StockQuiz.java
@@ -1,4 +1,32 @@
 package com.bull4jo.kkanbustock.quiz.domain.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
 public class StockQuiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String content;
+
+    @Column
+    private String answer;
+
+    @Column
+    private String explanation;
+
+    public StockQuiz(Long id, String content, String answer, String explanation) {
+        this.id = id;
+        this.content = content;
+        this.answer = answer;
+        this.explanation = explanation;
+    }
 }
