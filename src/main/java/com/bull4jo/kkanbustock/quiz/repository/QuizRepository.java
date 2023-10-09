85e41b612292071f1323d4e4fd95da16e04c2473
2522001 <minseo770@gmail.com>
2023-10-09 20:34:46 +0900

    [FEAT] QuizRepository 정의 (#3)

--- src/main/java/com/bull4jo/kkanbustock/quiz/repository/QuizRepository.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/repository/QuizRepository.java
@@ -1,4 +1,9 @@
 package com.bull4jo.kkanbustock.quiz.repository;
 
-public class QuizRepository {
+import com.bull4jo.kkanbustock.quiz.domain.entity.StockQuiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository extends JpaRepository<StockQuiz, Long> {
 }
