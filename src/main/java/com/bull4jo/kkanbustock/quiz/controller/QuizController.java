7547eb17306bd5c635641b684ebfaac1549c5560
2522001 <minseo770@gmail.com>
2023-10-10 00:40:18 +0900

    [FEAT] QuizController 정의 (#3)

--- src/main/java/com/bull4jo/kkanbustock/quiz/controller/QuizController.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/controller/QuizController.java
@@ -1,4 +1,35 @@
 package com.bull4jo.kkanbustock.quiz.controller;
 
+import com.bull4jo.kkanbustock.common.config.Response;
+import com.bull4jo.kkanbustock.quiz.domain.request.QuizRequest;
+import com.bull4jo.kkanbustock.quiz.domain.response.QuizResponse;
+import com.bull4jo.kkanbustock.quiz.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class QuizController {
+
+    private final QuizService quizService;
+
+    @GetMapping("/v1/quizzes")
+    public Response<List<QuizResponse>> getQuizzes() {
+        return Response.of(quizService.getQuizzes());
+    }
+
+    @GetMapping("/v1/quizzes/{quizId}")
+    public Response<QuizResponse> getQuiz(
+            @PathVariable(value = "quizId") long id
+    ) {
+        return Response.of(quizService.getQuiz(id));
+    }
+
+    @PostMapping("/v1/quizzes")
+    public void saveSolvedQuiz(@RequestBody QuizRequest quizRequest) {
+        quizService.saveSolvedQuiz(quizRequest);
+    }
 }
