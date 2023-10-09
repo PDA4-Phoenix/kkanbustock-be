172969ed96a44882a95937cc19ef7706addf6de3
2522001 <minseo770@gmail.com>
2023-10-10 00:38:10 +0900

    [FEAT] Quiz response, request 정의 (#3)

--- src/main/java/com/bull4jo/kkanbustock/quiz/domain/request/QuizRequest.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/domain/request/QuizRequest.java
@@ -1,4 +1,15 @@
 package com.bull4jo.kkanbustock.quiz.domain.request;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Getter
 public class QuizRequest {
+    private final Long memberId;
+    private final Long stockQuizId;
+    private final Date solveDate;
+    private final Boolean isCorrect;
 }
