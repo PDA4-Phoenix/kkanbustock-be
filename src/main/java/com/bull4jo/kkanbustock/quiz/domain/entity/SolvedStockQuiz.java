6cc5f0c9a8c78fc2d061736c89437a0f920d9950
2522001 <minseo770@gmail.com>
2023-10-09 17:14:33 +0900

    [FEAT] SolvedStockQuiz entity 정의 (#3)

--- src/main/java/com/bull4jo/kkanbustock/quiz/domain/entity/SolvedStockQuiz.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/domain/entity/SolvedStockQuiz.java
@@ -1,4 +1,40 @@
 package com.bull4jo.kkanbustock.quiz.domain.entity;
 
+import com.bull4jo.kkanbustock.member.domain.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
 public class SolvedStockQuiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn
+    private StockQuiz stockQuiz;
+
+    @Column
+    private Date solvedDate;
+
+    @Column
+    private Boolean isCorrect;
+
+    public SolvedStockQuiz(Member member, StockQuiz stockQuiz, Date solvedDate, Boolean isCorrect) {
+        this.member = member;
+        this.stockQuiz = stockQuiz;
+        this.solvedDate = solvedDate;
+        this.isCorrect = isCorrect;
+    }
 }
