f8efde72e0431e24742dad30e7558078605cb719
ghdeo <goldencorn7@naver.com>
2023-10-09 12:49:41 +0900

    init commit

---
+++ src/main/java/com/bull5jo/kkanbustock/KkanbustockApplication.java
@@ -0,0 +1,13 @@
+package com.bull5jo.kkanbustock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkanbustockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkanbustockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bull5jo/kkanbustock/KkanbustockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bull5jo.kkanbustock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkanbustockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
