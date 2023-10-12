cb25f3aae2ef915d96dd3644fdda2a73aa7f59f1
sangeun <grace0857@naver.com>
2023-10-12 19:07:36 +0900

    [FEAT]Google ë¡그인

---
+++ src/main/java/com/bull4jo/kkanbustock/login/domain/entity/Login.java
@@ -0,0 +1,14 @@
+package com.bull4jo.kkanbustock.login.domain.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Login {
+    String id;
+    String email;
+    String nickname;
+}
