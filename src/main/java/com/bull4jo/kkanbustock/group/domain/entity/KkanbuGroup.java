e1baa6cbda648c889a39b56decc7852d00f62c3c
2522001 <minseo770@gmail.com>
2023-10-12 21:59:49 +0900

    [FIX] Group 복합 기본 키 → 단일 기본 키 수정 (#13)

--- src/main/java/com/bull4jo/kkanbustock/group/domain/entity/KkanbuGroup.java
+++ src/main/java/com/bull4jo/kkanbustock/group/domain/entity/KkanbuGroup.java
@@ -1,10 +1,12 @@
 package com.bull4jo.kkanbustock.group.domain.entity;
 
 import com.bull4jo.kkanbustock.member.domain.entity.Member;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,12 +15,16 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class KkanbuGroup {
     @Id
-    @OneToOne
-    private Member hostId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Id
     @OneToOne
-    private Member guestId;
+    @JoinColumn(name = "host_id")
+    private Member host;
+
+    @OneToOne
+    @JoinColumn(name = "guest_id")
+    private Member guest;
 
     @Column
     private float profitRate;
@@ -31,4 +37,15 @@ public class KkanbuGroup {
 
     @Column(nullable = false)
     private LocalDateTime createdDate;
+
+    @Builder
+    public KkanbuGroup(Long id, Member host, Member guest, float profitRate, String inviteCode, String name, LocalDateTime createdDate) {
+        this.id = id;
+        this.host = host;
+        this.guest = guest;
+        this.profitRate = profitRate;
+        this.inviteCode = inviteCode;
+        this.name = name;
+        this.createdDate = createdDate;
+    }
 }
