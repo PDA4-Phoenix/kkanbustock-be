552cba2d899ea612c3e5761ed5d58eb6a9a094ca
2522001 <minseo770@gmail.com>
2023-10-12 17:49:44 +0900

    [FEAT] Group Repository 정의 (#12)

--- src/main/java/com/bull4jo/kkanbustock/group/repository/GroupRepository.java
+++ src/main/java/com/bull4jo/kkanbustock/group/repository/GroupRepository.java
@@ -1,4 +1,9 @@
 package com.bull4jo.kkanbustock.group.repository;
 
-public class GroupRepository {
+import com.bull4jo.kkanbustock.group.domain.entity.KkanbuGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<KkanbuGroup,Long> {
 }
