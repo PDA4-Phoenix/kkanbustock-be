1ad74885211185509ab7b6d1a95079cb8e351f22
ghdeo <goldencorn7@naver.com>
2023-10-12 21:14:02 +0900

    [FEAT] 주식 엔티티 추가 (#13)

---
+++ src/main/java/com/bull4jo/kkanbustock/stock/domain/MarketType.java
@@ -0,0 +1,7 @@
+package com.bull4jo.kkanbustock.stock.domain;
+
+public enum MarketType {
+    KOSPI,
+    KOSDAQ,
+    KONEX
+}
---
+++ src/main/java/com/bull4jo/kkanbustock/stock/domain/Stock.java
@@ -0,0 +1,59 @@
+package com.bull4jo.kkanbustock.stock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+    /**
+     * 종목 코드보다 짧으면서 유일성이 보장되는 코드(6자리)
+     * */
+    @Id
+    private String srtnCd;
+
+    /**
+     * 종목의 명칭
+     * */
+    @Column
+    private String itmsNm;
+
+    /**
+     * 주식의 시장 구분 (KOSPI/KOSDAQ/KONEX 중 1)
+     * */
+    @Column
+    private MarketType mrktCtg;
+
+    /**
+     * 정규시장의 매매시간 종료시까지 형성되는 최종가격
+     */
+    @Column
+    private int clpr;
+
+    /**
+     * 정규시장의 매매시간
+     * 개시 후 형성되는 최초가격
+     */
+    @Column
+    private int mkp;
+
+    /**
+     * 기준 일자
+     */
+    @Column
+    private int basDt;
+
+    public void update(int clpr, int mkp) {
+        this.clpr = clpr;
+        this.mkp = mkp;
+    }
+}
