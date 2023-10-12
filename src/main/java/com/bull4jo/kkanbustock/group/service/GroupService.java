4d78ace0297b574dda5edeb8c79f3d55d57e1f08
2522001 <minseo770@gmail.com>
2023-10-12 20:55:37 +0900

    [DEL] 불필요한 코드 삭제

--- src/main/java/com/bull4jo/kkanbustock/group/service/GroupService.java
+++ src/main/java/com/bull4jo/kkanbustock/group/service/GroupService.java
@@ -1,5 +1,8 @@
 package com.bull4jo.kkanbustock.group.service;
 
+import com.bull4jo.kkanbustock.group.controller.dto.GroupRequest;
+import com.bull4jo.kkanbustock.group.controller.dto.GroupResponse;
+import com.bull4jo.kkanbustock.group.domain.entity.KkanbuGroup;
 import com.bull4jo.kkanbustock.group.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +13,7 @@ public class GroupService {
 
     private final GroupRepository groupRepository;
 
-    public
+    public GroupRequest setGroupName(String name) {
+
+    }
 }
