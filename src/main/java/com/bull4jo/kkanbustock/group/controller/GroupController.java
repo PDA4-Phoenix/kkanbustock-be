99ae44b580f145be290dca211e3409799c730caa
ghdeo <goldencorn7@naver.com>
2023-10-13 19:28:24 +0900

    [FEAT] GroupController 그룹 수익률 계산 추가 (#12)

--- src/main/java/com/bull4jo/kkanbustock/group/controller/GroupController.java
+++ src/main/java/com/bull4jo/kkanbustock/group/controller/GroupController.java
@@ -1,4 +1,23 @@
 package com.bull4jo.kkanbustock.group.controller;
 
+import com.bull4jo.kkanbustock.group.domain.entity.KkanbuGroupPK;
+import com.bull4jo.kkanbustock.group.service.GroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class GroupController {
+    private final GroupService groupService;
+
+    @GetMapping("/v1/groups/profits")
+    public float getGroupProfitRate(@RequestParam KkanbuGroupPK kkanbuGroupPK) {
+        return groupService.getGroupProfitRate(kkanbuGroupPK);
+    }
+
+
 }
