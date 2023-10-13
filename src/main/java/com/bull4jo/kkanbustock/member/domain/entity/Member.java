42ef9f8ea22c48d7eb76e8647e8242415451e00e
ghdeo <goldencorn7@naver.com>
2023-10-13 19:21:27 +0900

    [FEAT] 멤버 도메인 포트폴리오 관련 칼럼 추가(#11)

--- src/main/java/com/bull4jo/kkanbustock/member/domain/entity/Member.java
+++ src/main/java/com/bull4jo/kkanbustock/member/domain/entity/Member.java
@@ -1,7 +1,9 @@
 package com.bull4jo.kkanbustock.member.domain.entity;
 
 import com.bull4jo.kkanbustock.group.domain.entity.KkanbuGroup;
+import com.bull4jo.kkanbustock.portfolio.domain.Portfolio;
 import com.bull4jo.kkanbustock.quiz.domain.entity.SolvedStockQuiz;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,9 @@ import java.util.List;
 @Entity
 @Table
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     @Id
     private String id;
@@ -25,19 +29,29 @@ public class Member {
 
     @Column
     private InvestorType investorType;
+    @Column
+    private float profitRate;
 
     @OneToMany(mappedBy = "member")
     private List<SolvedStockQuiz> solvedStockQuizzes;
 
-    @OneToMany(mappedBy = "member")
-    private List<KkanbuGroup> kkanbuGroups;
+    @OneToMany(mappedBy = "host")
+    private List<KkanbuGroup> hostKkanbuGroups;
 
-    public Member(Long id, String password, String nickname, InvestorType investorType, List<SolvedStockQuiz> solvedStockQuizzes, List<KkanbuGroup> kkanbuGroups) {
-        this.id = id;
-        this.email = email;
-        this.nickname = nickname;
-        this.investorType = investorType;
-        this.solvedStockQuizzes = solvedStockQuizzes;
-        this.kkanbuGroups = kkanbuGroups;
+    @OneToMany(mappedBy = "guest")
+    private List<KkanbuGroup> guestKkanbuGroups;
+
+    @OneToMany(mappedBy = "member")
+    private List<Portfolio> portfolios;
+
+    public void calculateTotalProfit() {
+        float totalPurchaseAmount = 0;
+        float totalEquitiesValue = 0;
+        for (Portfolio portfolio : portfolios) {
+            totalPurchaseAmount += portfolio.getPurchaseAmount();
+            totalEquitiesValue += portfolio.getEquitiesValue();
+        }
+        // 0 예외 처리
+        this.profitRate = (totalEquitiesValue / totalPurchaseAmount - 1) * 100;
     }
 }
