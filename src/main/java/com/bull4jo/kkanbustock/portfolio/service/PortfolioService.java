759b6a6ce14d95e252af2515bce1cacf1fe6c53d
ghdeo <goldencorn7@naver.com>
2023-10-13 10:59:42 +0900

    [FEAT] PortfolioService에 멤버 포트폴리오 지정 메서드 추가 (#14)

--- src/main/java/com/bull4jo/kkanbustock/portfolio/service/PortfolioService.java
+++ src/main/java/com/bull4jo/kkanbustock/portfolio/service/PortfolioService.java
@@ -1,8 +1,12 @@
 package com.bull4jo.kkanbustock.portfolio.service;
 
+import com.bull4jo.kkanbustock.member.domain.entity.Member;
+import com.bull4jo.kkanbustock.member.repository.MemberRepository;
 import com.bull4jo.kkanbustock.portfolio.domain.Portfolio;
 import com.bull4jo.kkanbustock.portfolio.domain.PortfolioPK;
 import com.bull4jo.kkanbustock.portfolio.repository.PortfolioRepository;
+import com.bull4jo.kkanbustock.stock.domain.Stock;
+import com.bull4jo.kkanbustock.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +17,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
+    private final MemberRepository memberRepository;
+    private final StockRepository stockRepository;
+
 
     // 포트폴리오 단일 조회
     @Transactional(readOnly = true)
@@ -50,5 +57,29 @@ public class PortfolioService {
 
         float profitRate = portfolioDenom / investedAsset;
         return profitRate;
+    }
+
+    @Transactional
+    public String addPortfolio(final String memberId, final String stockId, final float purchasePrice, final int quantity) {
+        Member member = memberRepository.findById(memberId).orElseThrow();
+        Stock stock = stockRepository.findById(stockId).orElseThrow();
+
+
+        Portfolio portfolio = Portfolio.builder()
+                .portfolioPK(
+                        PortfolioPK.builder()
+                                .memberId(memberId)
+                                .stockId(stockId)
+                                .build()
+                )
+                .member(member)
+                .stock(stock)
+                .purchasePrice(purchasePrice)
+                .quantity(quantity)
+                .build();
+        portfolio.setDerivedAttributes();
+
+        System.out.println(portfolio);
+        return portfolioRepository.save(portfolio).getPortfolioPK().getMemberId();
     }
 }
