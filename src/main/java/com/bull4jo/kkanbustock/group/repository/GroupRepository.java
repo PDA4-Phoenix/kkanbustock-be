caf6c8cc27b5545d9696b966c36c6cf52fecd7d8
2522001 <minseo770@gmail.com>
2023-10-14 02:26:56 +0900

    [DEL] 불필요한 코드 삭제 (#12)

--- src/main/java/com/bull4jo/kkanbustock/group/repository/GroupRepository.java
+++ src/main/java/com/bull4jo/kkanbustock/group/repository/GroupRepository.java
@@ -1,6 +1,7 @@
 package com.bull4jo.kkanbustock.group.repository;
 
 import com.bull4jo.kkanbustock.group.domain.entity.KkanbuGroup;
+import com.bull4jo.kkanbustock.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,4 @@ import java.util.Optional;
 @Repository
 public interface GroupRepository extends JpaRepository<KkanbuGroup,Long> {
     Optional<KkanbuGroup> findByName(String name);
-    Optional<KkanbuGroup> findByInviteCode(String inviteCode);
-
 }
