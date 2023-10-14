db5a23adcc75fa91cd487dccc1e6dfc129c5e012
2522001 <minseo770@gmail.com>
2023-10-14 21:22:53 +0900

    [FIX] 빌더 패턴 적용 (#17)

--- src/main/java/com/bull4jo/kkanbustock/quiz/controller/dto/QuizResponse.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/controller/dto/QuizResponse.java
@@ -1,6 +1,7 @@
 package com.bull4jo.kkanbustock.quiz.controller.dto;
 
 import com.bull4jo.kkanbustock.quiz.domain.entity.StockQuiz;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,12 +13,11 @@ public class QuizResponse {
     private final String answer;
     private final String explanation;
 
-    public static QuizResponse of(StockQuiz entity) {
-        return new QuizResponse(
-                entity.getId(),
-                entity.getContent(),
-                entity.getAnswer(),
-                entity.getExplanation()
-        );
+    @Builder
+    public QuizResponse(StockQuiz stockQuiz) {
+        this.id = stockQuiz.getId();
+        this.content = stockQuiz.getContent();
+        this.answer = stockQuiz.getAnswer();
+        this.explanation = stockQuiz.getExplanation();
     }
 }
