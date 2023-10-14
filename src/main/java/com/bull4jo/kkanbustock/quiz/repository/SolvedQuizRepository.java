2fecca1e28ffe906a94dd956a7fb0fec1f91f769
2522001 <minseo770@gmail.com>
2023-10-15 02:41:28 +0900

    [DEL] 불필요한 코드 삭제 (#17)

--- src/main/java/com/bull4jo/kkanbustock/quiz/repository/SolvedQuizRepository.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/repository/SolvedQuizRepository.java
@@ -4,8 +4,6 @@ import com.bull4jo.kkanbustock.quiz.domain.entity.SolvedStockQuiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SolvedQuizRepository extends JpaRepository<SolvedStockQuiz, Long> {
 }
