4a18808306b427621b6555fd95d972a84a76de9a
2522001 <minseo770@gmail.com>
2023-10-15 00:32:49 +0900

    [DEL] 불필요한 코드 삭제 (#17)

--- src/main/java/com/bull4jo/kkanbustock/quiz/controller/dto/QuizResponse.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/controller/dto/QuizResponse.java
@@ -3,10 +3,8 @@ package com.bull4jo.kkanbustock.quiz.controller.dto;
 import com.bull4jo.kkanbustock.quiz.domain.entity.StockQuiz;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class QuizResponse {
     private final Long id;
     private final String content;
