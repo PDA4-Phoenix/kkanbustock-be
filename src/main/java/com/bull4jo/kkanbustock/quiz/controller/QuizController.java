c830a41d0037b8b91a3474736543c64530b1c5a1
2522001 <minseo770@gmail.com>
2023-10-15 02:17:34 +0900

    [FIX] Quiz Controller 수정 (#17)

--- src/main/java/com/bull4jo/kkanbustock/quiz/controller/QuizController.java
+++ src/main/java/com/bull4jo/kkanbustock/quiz/controller/QuizController.java
@@ -1,8 +1,9 @@
 package com.bull4jo.kkanbustock.quiz.controller;
 
-import com.bull4jo.kkanbustock.quiz.controller.dto.DailyQuizRequest;
 import com.bull4jo.kkanbustock.quiz.controller.dto.QuizRequest;
-import com.bull4jo.kkanbustock.quiz.controller.dto.QuizResponse;
+import com.bull4jo.kkanbustock.quiz.controller.dto.SolvedQuizRequest;
+import com.bull4jo.kkanbustock.quiz.controller.dto.DailyQuizResponse;
+import com.bull4jo.kkanbustock.quiz.controller.dto.SolvedStockQuizResponse;
 import com.bull4jo.kkanbustock.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +18,22 @@ public class QuizController {
 
     private final QuizService quizService;
 
-    @GetMapping("/v1/quizzes")
-    public ResponseEntity<QuizResponse> getDailyQuiz(
-            @RequestBody DailyQuizRequest dailyQuizRequest
+    @GetMapping("/v1/quizzes/daily")
+    public ResponseEntity<DailyQuizResponse> getDailyQuiz(
+            @RequestBody QuizRequest quizRequest
     ) {
-        return ResponseEntity.ok(quizService.getDailyQuiz(dailyQuizRequest));
+        return ResponseEntity.ok(quizService.getDailyQuiz(quizRequest));
     }
 
     @GetMapping("/v1/quizzes")
-    public ResponseEntity<List<QuizResponse>> getQuizzes() {
-        return ResponseEntity.ok(quizService.getQuizzes());
+    public ResponseEntity<SolvedStockQuizResponse> getSolvedQuizzes(
+            @RequestBody QuizRequest quizRequest
+    ) {
+        return ResponseEntity.ok(quizService.getSolvedQuizzes(quizRequest));
     }
 
-    @PostMapping("/v1/quizzes")
-    public void saveSolvedQuiz(@RequestBody QuizRequest quizRequest) {
-        quizService.saveSolvedQuiz(quizRequest);
+    @PostMapping("/v1/quizzes/daily")
+    public void saveSolvedQuiz(@RequestBody SolvedQuizRequest solvedQuizRequest) {
+        quizService.saveSolvedQuiz(solvedQuizRequest);
     }
 }
