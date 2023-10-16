fb3471ccfcd275f260881ff3a44e5816438855ea
ghdeo <goldencorn7@naver.com>
2023-10-17 01:21:24 +0900

    [FEAT] Portfolio 에러메세지 추가 (#20)

--- src/main/java/com/bull4jo/kkanbustock/exception/ErrorCode.java
+++ src/main/java/com/bull4jo/kkanbustock/exception/ErrorCode.java
@@ -20,6 +20,7 @@ public enum ErrorCode {
     DAILY_QUIZ_FORBIDDEN(HttpStatus.FORBIDDEN, "오늘의 퀴즈를 이미 풀었습니다."),
 
     GROUP_APPLICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "같은 깐부 신청이 존재합니다."),
+
     GROUP_FORBIDDEN(HttpStatus.FORBIDDEN, "이미 깐부 관계입니다."),
 
     /*
@@ -28,12 +29,24 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버 정보를 찾을 수 없습니다."),
 
     QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND, "퀴즈 정보를 찾을 수 없습니다."),
+
     UNSOLVED_QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND, "풀지 않은 퀴즈 정보를 찾을 수 없습니다."),
+
     SOLVED_QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND, "푼 퀴즈 정보를 찾을 수 없습니다."),
+
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "깐부 정보를 찾을 수 없습니다."),
 
-    GROUP_APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "받은 깐부 신청을 찾을 수 없습니다."),
     MY_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "내 깐부 정보를 찾을 수 없습니다"),
+
+    GROUP_APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "받은 깐부 신청을 찾을 수 없습니다."),
+
+    CANT_CALCULATE_TOTAL_PURCHASE_AMOUNT(HttpStatus.NOT_FOUND, "멤버 포트폴리오의 매수 금액을 계산 할 수 없습니다."),
+
+    CANT_CALCULATE_EQUITIES_VALUE_AMOUNT(HttpStatus.NOT_FOUND, "멤버 포트폴리오의 평가금액을 계산 할 수 없습니다."),
+
+    PORTFOLIO_NOT_FOUND(HttpStatus.NOT_FOUND, "포트폴리오를 찾을 수 없습니다."),
+
+    STOCK_NOT_COUND(HttpStatus.NOT_FOUND, "주식 정보를 찾을 수 없습니다."),
 
     /*
      * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
