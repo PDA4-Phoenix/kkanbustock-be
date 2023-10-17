fda938aae1bc92876234c27dd9e8018b6928bcae
2522001 <minseo770@gmail.com>
2023-10-17 16:52:06 +0900

    [CHORE] NewsResponse에 id 추가 (#38)

--- src/main/java/com/bull4jo/kkanbustock/news/controller/dto/NewsResponse.java
+++ src/main/java/com/bull4jo/kkanbustock/news/controller/dto/NewsResponse.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class NewsResponse {
+    private final Long id;
     private final String title;
     private final String link;
     private final String description;
@@ -13,6 +14,7 @@ public class NewsResponse {
 
     @Builder
     public NewsResponse(News news) {
+        this.id = news.getId();
         this.title = news.getTitle();
         this.link = news.getLink();
         this.description = news.getDescription();
