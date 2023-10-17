502ecad51e7b0ee336f9306e3d84ecfb5b740d28
2522001 <minseo770@gmail.com>
2023-10-17 16:03:32 +0900

    [FEAT] 뉴스 컨트롤러 정의 (#38)

--- src/main/java/com/bull4jo/kkanbustock/news/controller/NewsController.java
+++ src/main/java/com/bull4jo/kkanbustock/news/controller/NewsController.java
@@ -1,4 +1,28 @@
 package com.bull4jo.kkanbustock.news.controller;
 
+import com.bull4jo.kkanbustock.news.controller.dto.NewsResponse;
+import com.bull4jo.kkanbustock.news.service.NewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class NewsController {
+
+    private final NewsService newsService;
+
+    @GetMapping("/v1/news")
+    public ResponseEntity<List<NewsResponse>> getNews(
+            @RequestParam(value = "keyword") String keyword,
+            @RequestParam(value = "size") int size
+    ) {
+        return ResponseEntity.ok(newsService.getNews(keyword, size));
+    }
 }
