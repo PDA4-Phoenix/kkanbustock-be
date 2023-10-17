ffab387688ed0742bb9d1eca0e2386d18ba2a141
2522001 <minseo770@gmail.com>
2023-10-17 16:41:34 +0900

    [FEAT] 뉴스 엔티티 정의 (#38)

--- src/main/java/com/bull4jo/kkanbustock/news/domain/entity/News.java
+++ src/main/java/com/bull4jo/kkanbustock/news/domain/entity/News.java
@@ -2,21 +2,46 @@ package com.bull4jo.kkanbustock.news.domain.entity;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Builder
+import javax.persistence.*;
+
+@Entity
+@Table
 @Getter
+@NoArgsConstructor
 @ToString
 public class News {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
     private String title;
+
+    @Column
     private String link;
+
+    @Column
     private String description;
+
+    @Column
     private String pubDate;
+
+    @Builder
+    public News(String title, String link, String description, String pubDate) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubDate = pubDate;
+    }
 
     public static String replace(String text){
         return text.replace("<b>", "")
                 .replace("</b>", "")
                 .replace("&quot;", "\"");
     }
+
 }
