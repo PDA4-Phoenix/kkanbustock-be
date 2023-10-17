a77bddb09fc647a65c1837a44d07d4121f4e26a5
ghdeo <goldencorn7@naver.com>
2023-10-17 16:41:12 +0900

    [FEAT] 랜덤 주식 추출 메서드 추가 (#47)

--- src/main/java/com/bull4jo/kkanbustock/stock/repository/StockRepository.java
+++ src/main/java/com/bull4jo/kkanbustock/stock/repository/StockRepository.java
@@ -2,10 +2,15 @@ package com.bull4jo.kkanbustock.stock.repository;
 
 import com.bull4jo.kkanbustock.stock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, String> {
 
     Optional<Stock> findStockByItmsNm(String itmsNum);
+
+    @Query(value = "SELECT * FROM stock order by RAND() 20",nativeQuery = true)
+    Optional<List<Stock>> findRandomStocks();
 }
