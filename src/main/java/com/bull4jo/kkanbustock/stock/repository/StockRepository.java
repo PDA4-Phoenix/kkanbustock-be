ba87e2bb5275fc7f8b9f505dcb39eeac650af4b6
ghdeo <goldencorn7@naver.com>
2023-10-19 10:26:10 +0900

    [FEAT] StockRepository 종목 분류 쿼리작성 (#78)

--- src/main/java/com/bull4jo/kkanbustock/stock/repository/StockRepository.java
+++ src/main/java/com/bull4jo/kkanbustock/stock/repository/StockRepository.java
@@ -16,4 +16,13 @@ public interface StockRepository extends JpaRepository<Stock, String> {
 
     @Query(value = "SELECT * FROM stock ORDER BY mrkt_tot_amt DESC LIMIT 500", nativeQuery = true)
     Optional<List<Stock>> findTopByMrktTotAmt();
+
+    @Query("SELECT s FROM Stock s WHERE (s.fltRt >= 10 AND s.trPrc >= 50000000000)")
+    Optional<List<Stock>> findAggressiveStocks();
+
+    @Query("SELECT s FROM Stock s WHERE (s.fltRt <= 5 AND s.fltRt >= -5 AND s.mrktTotAmt >= 50000000000)")
+    Optional<List<Stock>> findModerateStock();
+
+    @Query("SELECT s FROM Stock s WHERE (s.fltRt <= 3 AND s.fltRt >= -3 AND s.mrktTotAmt >= 1000000000000)")
+    Optional<List<Stock>> findConservativeStock();
 }
