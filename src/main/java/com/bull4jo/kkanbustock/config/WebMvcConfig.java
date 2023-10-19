169c8b2d34e80324ac580cb5246bcb192c0dafb5
ghdeo <goldencorn7@naver.com>
2023-10-20 01:56:01 +0900

    [FEAT] WebMvcConfig 작성

---
+++ src/main/java/com/bull4jo/kkanbustock/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.bull4jo.kkanbustock.config;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*") // 기타 설정
+                .allowedHeaders("*");
+    }
+}
